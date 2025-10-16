682aa87e5496e04aa751e725761799f1ef4b0d98
Sofia20092003 <angelasofiarodriguezarcinegas03@liceolapaz.net>
2025-10-16 12:53:08 +0200

    Ejercicio Analizador de Números Terminado

--- src/analizadordenumeros/analizadordenumeros.java
+++ src/analizadordenumeros/analizadordenumeros.java
@@ -1,4 +1,44 @@
 package analizadordenumeros;
 
+import java.util.Scanner;
+
 public class analizadordenumeros {
+    public static void main(String[] args) {
+        int opcion;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("*****************************************************************");
+        System.out.println("****************BIENVENIDO AL ANALIZADOR DE NÚMEROS**************");
+        System.out.println("*****************************************************************");
+
+        do {
+            int menor=Integer.MIN_VALUE;
+            int mayor=Integer.MAX_VALUE;
+            boolean primernumero= true;
+            for (int i=1; i<=5; i++){
+                System.out.println("Introduce los números que quieras comparar: ");
+                int numeroactual=sc.nextInt();
+                if(primernumero){
+                    menor=numeroactual;
+                    mayor=numeroactual;
+                    primernumero=false;
+                }else{
+                    if(numeroactual < menor){
+                        menor=numeroactual;
+                    }
+                    if(numeroactual > mayor){
+                        mayor=numeroactual;
+                    }
+                }
+            }
+            System.out.println("El numero mayor es: "+mayor);
+            System.out.println("El numero menor es: "+menor);
+            System.out.println("*****************************************************************");
+            System.out.println("Desea analizar otro grupo de 5 numeros? \n" +
+                    "1. si \n" +
+                    "2. No");
+            opcion=sc.nextInt();
+        }
+        while (opcion!=2);
+        System.out.println("Programa finalizado.");
+    }
 }
